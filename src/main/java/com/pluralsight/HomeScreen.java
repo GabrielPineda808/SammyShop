7e4f00e350fa6d9ba033360b2c13adaf05b14357
Gabe <gabepineda6@gmail.com>
2024-11-08 07:48:02 -0800

    Created HomeScreen class to start the application. Prompted the user to input one of two options after starting the application

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
+
+import static com.pluralsight.GlobalMethods.ans;
+
 public class HomeScreen {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our SammyShop\n");
+
+        String option = ans("Please select one of the following options below\n" +
+                "( 1 ) - Create New Order\n" +
+                "( 2 ) - Exit Application\n");
+
+    }
 }
