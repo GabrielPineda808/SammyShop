bc8e7adaef6c18f99b61a3d84827951797df7c00
Gabe <gabepineda6@gmail.com>
2024-11-08 08:05:44 -0800

    Created a switch case that will either exit the app if the user chooses not to createa an order. if they choose to create an order we call on a makeSandwhich method that we will create later

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -2,14 +2,30 @@ package com.pluralsight;
 
 
 import static com.pluralsight.GlobalMethods.ans;
+import static java.lang.System.exit;
 
 public class HomeScreen {
-    public static void main(String[] args) {
+    public static void home(){
         System.out.println("Welcome to our SammyShop\n");
 
-        String option = ans("Please select one of the following options below\n" +
+        String option = ans(
+                "Please select one of the following options below\n" +
                 "( 1 ) - Create New Order\n" +
-                "( 2 ) - Exit Application\n");
+                "( 2 ) - Exit Application\n"
+        );
 
+        switch (option){
+            case "1":
+                makeSandwhich();
+                break;
+            case "2":
+                System.out.println("Thank you for visiting SammyShop! Have a great day!\n");
+                exit(1);
+                break;
+            default:
+                System.out.println("Unfortunately that was not a valid option. Try again.\n");
+                home();
+
+        }
     }
 }
