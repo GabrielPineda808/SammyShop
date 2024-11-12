fd6e571dd336630ff2d88de300a64ea65633ec5b
Gabe <gabepineda6@gmail.com>
2024-11-12 09:50:09 -0800

    changed input options

--- src/main/java/com/pluralsight/Size.java
+++ src/main/java/com/pluralsight/Size.java
@@ -6,17 +6,20 @@ public class Size {
     public static int size(){
         System.out.println("\n What size would you like your sandwich to be? Please enter an option below. \n");
 
-        int size = ans("( 4 ) - 4 inch ($5.50)\n" +
-                "( 8 ) - 8 inch ($7.00)\n" +
-                "( 12 ) - 12 inch ($8.50)\n");
+        int size = ans("( 1 ) - 4 inch ($5.50)\n" +
+                "( 2 ) - 8 inch ($7.00)\n" +
+                "( 3 ) - 12 inch ($8.50)\n");
 
         switch (size){
-            case 4:
+            case 1:
                 return 4;
-            case 8:
+            case 2:
                 return 8;
-            case 12:
+            case 3:
                 return 12;
+            default:
+                System.out.println("That was not a proper size choice. Please enter again :)\n");
+                size();
         }
 
         return size;
