720ea15a4906c7e4319148e855800209d9d4fb97
Gabe <gabepineda6@gmail.com>
2024-11-12 09:49:23 -0800

    Created methods to add regular toppingss, premium toppings and cheese into an arraylist that will then be added to the sandwich object

--- src/main/java/com/pluralsight/Toppings.java
+++ src/main/java/com/pluralsight/Toppings.java
@@ -6,61 +6,143 @@ import static com.pluralsight.GlobalMethods.ans;
 
 public class Toppings {
     static ArrayList<String> regularToppings = new ArrayList<>();
+    static ArrayList<String> premiumToppings = new ArrayList<>();
+    static ArrayList<String> cheeses = new ArrayList<>();
+
     public static ArrayList<String> regularToppings(){
-        loop: while (true){
-            int topping = ans("What regularToppings would you like on your sandwich? \n" +
+        System.out.println("\nPlease select your toppings below. They are free with your order!\n");
+        loop: while (true) {
+            int topping = ans("What toppings would you like on your sandwich? \n" +
                     "( 1 ) - Lettuce      ( 6 ) - Cucumbers\n" +
                     "( 2 ) - Peppers      ( 7 ) - Pickles\n" +
                     "( 3 ) - Onions       ( 8 ) - Guacamole\n" +
                     "( 4 ) - Tomatoes     ( 9 ) - Mushrooms\n" +
                     "( 5 ) - Jalapenos    ( 0 ) - Done with Toppings\n");
 
-            switch (topping){
+            switch (topping) {
                 case 1:
                     regularToppings.add("Lettuce");
-                    regularToppings();
                     break;
                 case 2:
                     regularToppings.add("Peppers");
-                    regularToppings();
                     break;
                 case 3:
                     regularToppings.add("Onions");
-                    regularToppings();
                     break;
                 case 4:
                     regularToppings.add("Tomatoes");
-                    regularToppings();
                     break;
                 case 5:
                     regularToppings.add("Jalapenos");
-                    regularToppings();
                     break;
                 case 6:
                     regularToppings.add("Cucumbers");
-                    regularToppings();
                     break;
                 case 7:
                     regularToppings.add("Pickles");
-                    regularToppings();
                     break;
                 case 8:
                     regularToppings.add("Guacamole");
-                    regularToppings();
                     break;
                 case 9:
                     regularToppings.add("Mushrooms");
-                    regularToppings();
                     break;
                 case 0:
                     System.out.println("All regular regularToppings have been added.");
                     break loop;
                 default:
-                    System.out.println("That was not a proper regularToppings choice. Enter again :)");
-                    regularToppings();
+                    System.out.println("That was not a proper topping choice. Enter again :)");
+            }
+        }
+        return regularToppings;
+    }
+
+    public static ArrayList<String> premiumToppings(){
+        System.out.println("\nPlease select your premium toppings below. They do come at an additional cost.\n" +
+                "Pricing - 4 inch( $1.00 / $0.50 per extra ) - 8 inch( $2.00 / $1.00 per extra )- 8 inch( $3.00 / $1.50 per extra )\n");
+
+        loop: while (true){
+            int topping = ans("What regularToppings would you like on your sandwich? \n" +
+                    "( 1 ) - Steak      ( 6 ) - Bacon\n" +
+                    "( 2 ) - Ham        ( 0 ) - Done with Toppings\n" +
+                    "( 3 ) - Salami\n" +
+                    "( 4 ) - Roast Beef\n" +
+                    "( 5 ) - Chicken\n");
+
+            switch (topping){
+                case 1:
+                    premiumToppings.add("Steak");
+                    premiumToppings();
+                    break;
+                case 2:
+                    premiumToppings.add("Ham");
+                    premiumToppings();
+                    break;
+                case 3:
+                    premiumToppings.add("Salami");
+                    premiumToppings();
+                    break;
+                case 4:
+                    premiumToppings.add("Roast Beef");
+                    premiumToppings();
+                    break;
+                case 5:
+                    premiumToppings.add("Chicken");
+                    premiumToppings();
+                    break;
+                case 6:
+                    premiumToppings.add("Bacon");
+                    premiumToppings();
+                    break;
+                case 0:
+                    System.out.println("All premium toppings have been added.");
+                    break loop;
+                default:
+                    System.out.println("That was not a proper topping choice. Enter again :)");
+                    premiumToppings();
             }
         }
 
-        return regularToppings;
+        return premiumToppings;
+    }
+
+    public static ArrayList<String> cheese(){
+        System.out.println("\nPlease select your cheese below. Cheese comes at an additional cost.\n" +
+                "Pricing - 4 inch( $0.75 / $0.30 per extra ) - 8 inch( $1.50 / $0.60 per extra )- 8 inch( $2.25 / $0.90 per extra )\n");
+
+        loop: while (true){
+            int c = ans("What regularToppings would you like on your sandwich? \n" +
+                    "( 1 ) - American\n" +
+                    "( 2 ) - Provolone\n" +
+                    "( 3 ) - Cheddar\n" +
+                    "( 4 ) - Swiss\n" +
+                    "( 0 ) - Done with Toppings\n");
+
+            switch (c){
+                case 1:
+                    cheeses.add("American");
+                    cheese();
+                    break;
+                case 2:
+                    cheeses.add("Provolone");
+                    cheese();
+                    break;
+                case 3:
+                    cheeses.add("Cheddar");
+                    cheese();
+                    break;
+                case 4:
+                    cheeses.add("Swiss");
+                    cheese();
+                    break;
+                case 0:
+                    System.out.println("All cheese has been added.");
+                    break loop;
+                default:
+                    System.out.println("That was not a proper cheese choice. Enter again :)");
+                    cheese();
+            }
+        }
+        return cheeses;
     }
 }
