7ea601c9bffa414c99538dfdec94a473cb2904bd
Gabe <gabepineda6@gmail.com>
2024-11-12 08:44:17 -0800

    Changed constructor to only accept size and bread when creating the sandwhich object as we can add the toppings cheese and is toasted later on

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -5,14 +5,22 @@ import java.util.ArrayList;
 public class Sandwich {
     private String bread;
     private boolean isToasted = false;
+    private int size;
     private double price;
     ArrayList<String> regularTopping;
     ArrayList<String> premiumTopping;
 
-    public Sandwich(String bread, boolean isToasted, double price) {
+    public Sandwich(int size ,String bread) {
         this.bread = bread;
-        this.isToasted = isToasted;
-        this.price = price;
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
     public String getBread() {
@@ -35,8 +43,27 @@ public class Sandwich {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setPrice() {
+        switch (this.size){
+            case 4:
+                this.price+= 5.50;
+                if(!premiumTopping.isEmpty()){
+                    this.price +=1.00 + (premiumTopping.size() -1 * .50);
+                }
+                break;
+            case 8:
+                this.price+= 7.00;
+                if(!premiumTopping.isEmpty()){
+                    this.price +=2.00+ (premiumTopping.size() -1 * 1.00);
+                }
+                break;
+            case 12:
+                if(!premiumTopping.isEmpty()){
+                    this.price +=3.00+ (premiumTopping.size() -1 * 1.50);
+                }
+                this.price+= 8.50;
+                break;
+        }
     }
 
     public void addRegularToppings(ArrayList<String> toppings){
--- src/main/java/com/pluralsight/Toppings.java
+++ src/main/java/com/pluralsight/Toppings.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 import static com.pluralsight.GlobalMethods.ans;
 
-public class RegularToppings {
-    static ArrayList<String> toppings = new ArrayList<>();
+public class Toppings {
+    static ArrayList<String> regularToppings = new ArrayList<>();
     public static ArrayList<String> regularToppings(){
         loop: while (true){
-            int topping = ans("What toppings would you like on your sandwich? \n" +
+            int topping = ans("What regularToppings would you like on your sandwich? \n" +
                     "( 1 ) - Lettuce      ( 6 ) - Cucumbers\n" +
                     "( 2 ) - Peppers      ( 7 ) - Pickles\n" +
                     "( 3 ) - Onions       ( 8 ) - Guacamole\n" +
@@ -17,50 +17,50 @@ public class RegularToppings {
 
             switch (topping){
                 case 1:
-                    toppings.add("Lettuce");
+                    regularToppings.add("Lettuce");
                     regularToppings();
                     break;
                 case 2:
-                    toppings.add("Peppers");
+                    regularToppings.add("Peppers");
                     regularToppings();
                     break;
                 case 3:
-                    toppings.add("Onions");
+                    regularToppings.add("Onions");
                     regularToppings();
                     break;
                 case 4:
-                    toppings.add("Tomatoes");
+                    regularToppings.add("Tomatoes");
                     regularToppings();
                     break;
                 case 5:
-                    toppings.add("Jalapenos");
+                    regularToppings.add("Jalapenos");
                     regularToppings();
                     break;
                 case 6:
-                    toppings.add("Cucumbers");
+                    regularToppings.add("Cucumbers");
                     regularToppings();
                     break;
                 case 7:
-                    toppings.add("Pickles");
+                    regularToppings.add("Pickles");
                     regularToppings();
                     break;
                 case 8:
-                    toppings.add("Guacamole");
+                    regularToppings.add("Guacamole");
                     regularToppings();
                     break;
                 case 9:
-                    toppings.add("Mushrooms");
+                    regularToppings.add("Mushrooms");
                     regularToppings();
                     break;
                 case 0:
-                    System.out.println("All regular toppings have been added.");
+                    System.out.println("All regular regularToppings have been added.");
                     break loop;
                 default:
-                    System.out.println("That was not a proper toppings choice. Enter again :)");
+                    System.out.println("That was not a proper regularToppings choice. Enter again :)");
                     regularToppings();
             }
         }
 
-        return toppings;
+        return regularToppings;
     }
 }
