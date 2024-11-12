0ad2a64514732c401d124a592f403e8f332ba0d7
Gabe <gabepineda6@gmail.com>
2024-11-12 07:20:02 -0800

    Created a class for the order screen that will hold the prompt for the user. We then use a switch case to determine what method and class will be called next

--- src/main/java/com/pluralsight/OrderScreen.java
+++ src/main/java/com/pluralsight/OrderScreen.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
+import static com.pluralsight.GlobalMethods.ans;
+
 public class OrderScreen {
+    public static void order(){
+
+        System.out.println("\nHow may we help you today? Please select an option below :)\n" +
+                "( 1 ) - Add Sandwich\n" +
+                "( 2 ) - Add Drink\n" +
+                "( 3 ) - Add Chips\n" +
+                "( 4 ) - Checkout\n" +
+                "( 5 ) - Cancel Order\n");
+        String orderChoice = ans("Enter here: ");
+        switch (orderChoice){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            default:
+                System.out.println("\nThat is not a proper choice please enter again.");
+                order();
+        }
+
+
+    }
 }
