3d654420d87b76c678a5278e47683d45c3b02178
Gabe <gabepineda6@gmail.com>
2024-11-12 09:34:17 -0800

    Data type change from string to int when revceiving user input

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -9,17 +9,17 @@ public class HomeScreen {
     public static void home(){
         System.out.println("Welcome to our SammyShop\n");
 
-        String option = ans(
+        int option = ans(
                 "Please select one of the following options below\n" +
                 "( 1 ) - Create New Order\n" +
                 "( 2 ) - Exit Application\n"
         );
 
         switch (option){
-            case "1":
+            case 1:
                 order();
                 break;
-            case "2":
+            case 2:
                 System.out.println("Thank you for visiting SammyShop! Have a great day!\n");
                 exit(1);
                 break;
