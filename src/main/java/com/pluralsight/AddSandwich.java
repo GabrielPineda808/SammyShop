296d65404321810d5c9fe7157f16ca9c0c6157cf
Gabe <gabepineda6@gmail.com>
2024-11-12 09:52:54 -0800

    Created a sandwhich object that we are prompting the user to create after they chose to add a sandwich to their order

--- src/main/java/com/pluralsight/AddSandwich.java
+++ src/main/java/com/pluralsight/AddSandwich.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
+import static com.pluralsight.Bread.bread;
+import static com.pluralsight.Size.size;
+import static com.pluralsight.Toppings.*;
+
 public class AddSandwich {
+    public static void AddSammy(){
+        System.out.println("\nHey there! Lets go ahead and make that sandwich for you!\n");
+
+        Sandwich sandwich = new Sandwich(size(),bread());
+
+        sandwich.addRegularToppings(regularToppings());
+        sandwich.addPremiumToppings(premiumToppings());
+        sandwich.addCheese(cheese());
+
+
+    }
 }
