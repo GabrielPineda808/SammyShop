929697e9ed2335ce2fa416b3fbe3946071ab3e62
Gabe <gabepineda6@gmail.com>
2024-11-12 17:42:44 -0800

    Made the class an extension of an orderable class

--- src/main/java/com/pluralsight/AddSandwich.java
+++ src/main/java/com/pluralsight/AddSandwich.java
@@ -3,10 +3,12 @@ package com.pluralsight;
 import java.util.ArrayList;
 
 import static com.pluralsight.Bread.bread;
+import static com.pluralsight.GlobalMethods.ans;
+import static com.pluralsight.OrderScreen.order;
 import static com.pluralsight.Size.size;
 import static com.pluralsight.Toppings.*;
 
-public class AddSandwich {
+public class AddSandwich extends Orderable{
     static ArrayList<Sandwich> sandwiches = new ArrayList<>();
     public static void AddSammy(){
         System.out.println("\nHey there! Lets go ahead and make that sandwich for you!\n");
@@ -21,6 +23,24 @@ public class AddSandwich {
         sandwich.toast();
 
         sandwiches.add(sandwich);
+
+        loop : while (true){
+            System.out.println("\nWould you like to add another sandwich? \n");
+            int choice = ans("( 1 ) - Yes\n" +
+                    "( 2 ) - No\n");
+            switch (choice){
+                case 1:
+                    System.out.println("Time to make another sandwich :) \n");
+                    AddSammy();
+                    break loop;
+                case 2:
+                    System.out.println("Back to the order screen :)\n");
+                    order();
+                    break loop;
+                default:
+                    System.out.println("That was not a proper option. Enter again :)\n");
+            }
+        }
 
     }
 
