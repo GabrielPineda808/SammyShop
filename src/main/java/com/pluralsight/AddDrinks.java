b6594848f915b8ce0c5b56e7eeed1963ef6717ef
Gabe <gabepineda6@gmail.com>
2024-11-12 18:23:34 -0800

    Created a class to add a drink to the order and add it to a  list of drinks so that we can add multiple drinks to the order

--- src/main/java/com/pluralsight/AddDrinks.java
+++ src/main/java/com/pluralsight/AddDrinks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.pluralsight.GlobalMethods.ans;
+import static com.pluralsight.OrderScreen.order;
 
 public class AddDrinks {
     static List<Drinks> drinks = new ArrayList<>();
@@ -12,6 +13,23 @@ public class AddDrinks {
         Drinks drink = new Drinks(drinkSize(),drinkName());
 
         drinks.add(drink);
+        loop : while (true){
+            System.out.println("\nWould you like to add another drink? \n");
+            int choice = ans("( 1 ) - Yes\n" +
+                    "( 2 ) - No\n");
+            switch (choice){
+                case 1:
+                    System.out.println("Lets add another drink :) \n");
+                    addDrinks();
+                    break loop;
+                case 2:
+                    System.out.println("Back to the order screen :)\n");
+                    order();
+                    break loop;
+                default:
+                    System.out.println("That was not a proper option. Enter again :)\n");
+            }
+        }
     }
 
     public static String drinkSize(){
@@ -28,6 +46,35 @@ public class AddDrinks {
                 return "Large";
             default:
                 System.out.println("That was not a proper choice. Enter again :)\n");
+                drinkSize();
         }
+        return "";
+    }
+    public static String drinkName(){
+        int drinkSize = ans("Select a drink below \n" +
+                "( 1 ) - Coke          ( 5 ) - Coffee\n" +
+                "( 2 ) - Sprite         ( 6 ) - Tea\n" +
+                "( 3 ) - Dr. Pepper     ( 0 ) - Water\n" +
+                "( 4 ) - Orange Juice");
+        switch (drinkSize){
+            case 1:
+                return "Coke";
+            case 2:
+                return "Sprite";
+            case 3:
+                return "Dr. Pepper";
+            case 4:
+                return "Orange Juice";
+            case 5:
+                return "Coffee";
+            case 6:
+                return "Tea";
+            case 0:
+                return "Water";
+            default:
+                System.out.println("That was not a proper choice. Enter again :)\n");
+                drinkSize();
+        }
+        return "";
     }
 }
