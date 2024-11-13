092ff939bec0470654e28f950c0f2110db5d00cf
Gabe <gabepineda6@gmail.com>
2024-11-12 18:24:19 -0800

    Createda  chip object that can holda name and price

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
-public class Chips {
+public class Chips extends Orderable{
+    private String name;
+    private double price = 1.50;
+
+    public Chips(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
