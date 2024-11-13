a7e5983d946c0c0562e391aa9ae5ea7713e2254e
Gabe <gabepineda6@gmail.com>
2024-11-12 17:53:49 -0800

    Prompted the user ot add drinks. Created new drink object that will call on methods as paramters to have the usaer input drink saize and name

--- src/main/java/com/pluralsight/AddDrinks.java
+++ src/main/java/com/pluralsight/AddDrinks.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.pluralsight.GlobalMethods.ans;
+
 public class AddDrinks {
+    static List<Drinks> drinks = new ArrayList<>();
+    public static void addDrinks(){
+        System.out.println("\nHere we will add your drinks :)\n");
+        Drinks drink = new Drinks(drinkSize(),drinkName());
+
+        drinks.add(drink);
+    }
+
+    public static String drinkSize(){
+        int drinkSize = ans("Select a drink size below \n" +
+                "( 1 ) - Small ( $2.00 )\n" +
+                "( 2 ) - Medium ( $ 2.50 )\n" +
+                "( 3 ) - Large ( $3.00 )\n");
+        switch (drinkSize){
+            case 1:
+                return "Small";
+            case 2:
+                return "Medium";
+            case 3:
+                return "Large";
+            default:
+                System.out.println("That was not a proper choice. Enter again :)\n");
+        }
+    }
 }
