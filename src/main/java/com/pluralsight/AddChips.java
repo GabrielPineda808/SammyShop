7ab1a5ef269f34f8654f6c94203576da61610eba
Gabe <gabepineda6@gmail.com>
2024-11-12 18:22:37 -0800

    Created a class that will prompt the user to enter which chips they want that will then be saved to a chiops list so that we can keep adding chips to the order if need be

---
+++ src/main/java/com/pluralsight/AddChips.java
@@ -0,0 +1,62 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.pluralsight.GlobalMethods.ans;
+import static com.pluralsight.OrderScreen.order;
+
+public class AddChips {
+    static List<Chips> chips = new ArrayList<>();
+    public static void addChips(){
+        System.out.println("\nHere we will add your chips :) \n");
+
+        Chips chip = new Chips(chipsName());
+
+        chips.add(chip);
+
+        loop : while (true){
+            System.out.println("\nWould you like to more chips? \n");
+            int choice = ans("( 1 ) - Yes\n" +
+                    "( 2 ) - No\n");
+            switch (choice){
+                case 1:
+                    System.out.println("Lets add more chips :) \n");
+                    addChips();
+                    break loop;
+                case 2:
+                    System.out.println("Back to the order screen :)\n");
+                    order();
+                    break loop;
+                default:
+                    System.out.println("That was not a proper option. Enter again :)\n");
+            }
+        }
+    }
+
+    public static String chipsName(){
+        int chipsName = ans("Select chips below \n" +
+                "( 1 ) - Hot Cheetos        ( 4 ) - Fritos\n" +
+                "( 2 ) - Doritos            ( 5 ) - Lays\n" +
+                "( 3 ) - Hot Fries          ( 6 ) - Sun Chips\n");
+        switch (chipsName){
+            case 1:
+                return "Hot Cheetos";
+            case 2:
+                return "Doritos";
+            case 3:
+                return "Hot Fries";
+            case 4:
+                return "Fritos";
+            case 5:
+                return "Lays";
+            case 6:
+                return "Sun Chips";
+            default:
+                System.out.println("That was not a proper choice. Enter again :)\n");
+                chipsName();
+        }
+
+        return "";
+    }
+}
