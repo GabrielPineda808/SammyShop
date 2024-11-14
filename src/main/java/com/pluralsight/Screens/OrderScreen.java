103c697286377bc503db369a9bdee9fad57582b4
Gabe <gabepineda6@gmail.com>
2024-11-14 05:50:07 -0800

    Placed prompt inside of a while loop to ensure user made valid choice

--- src/main/java/com/pluralsight/Screens/OrderScreen.java
+++ src/main/java/com/pluralsight/Screens/OrderScreen.java
@@ -1,9 +1,6 @@
 package com.pluralsight.Screens;
 
-import com.pluralsight.adding.AddDrinks;
-
 import java.io.IOException;
-
 import static com.pluralsight.Screens.Checkout.checkout;
 import static com.pluralsight.adding.AddChips.addChips;
 import static com.pluralsight.adding.AddDrinks.addDrinks;
@@ -13,35 +10,33 @@ import static com.pluralsight.GlobalMethods.ans;
 public class OrderScreen {
     public static void order() throws IOException {
 
-        System.out.println("\nHow may we help you today? Please select an option below :)\n" +
-                "\n( 1 ) - Add Sandwich\n" +
-                "( 2 ) - Add Drink\n" +
-                "( 3 ) - Add Chips\n" +
-                "( 4 ) - Checkout\n" +
-                "( 5 ) - Cancel Order\n");
-        int orderChoice = ans("Enter here: \n");
-        switch (orderChoice){
-            case 1:
-                AddSammy();
-                break;
-            case 2:
-                addDrinks();
-                break;
-            case 3:
-                addChips();
-                break;
-            case 4:
-                checkout();
-                break;
-            case 5:
-
-
-                break;
-            default:
-                System.out.println("\nThat is not a proper choice please enter again.");
-                order();
+        loop : while (true) {
+            System.out.println("\nHow may we help you today? Please select an option below :)\n" +
+                    "\n( 1 ) - Add Sandwich\n" +
+                    "( 2 ) - Add Drink\n" +
+                    "( 3 ) - Add Chips\n" +
+                    "( 4 ) - Checkout\n" +
+                    "( 5 ) - Cancel Order\n");
+            int orderChoice = ans("Enter here: \n");
+            switch (orderChoice) {
+                case 1:
+                    AddSammy();
+                    break loop;
+                case 2:
+                    addDrinks();
+                    break loop;
+                case 3:
+                    addChips();
+                    break loop;
+                case 4:
+                    checkout();
+                    break loop;
+                case 5:
+                    Cancel.Cancel();
+                    break loop;
+                default:
+                    System.out.println("\nThat is not a proper choice please enter again.");
+            }
         }
-
-
     }
 }
