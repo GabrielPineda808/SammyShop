63a7d64eb8b6c584898e94ea012c34c0ec36b00c
Gabe <gabepineda6@gmail.com>
2024-11-14 05:44:24 -0800

    Created a safety measure that ensure the input is both a valid integer and an option available

--- src/main/java/com/pluralsight/GlobalMethods.java
+++ src/main/java/com/pluralsight/GlobalMethods.java
@@ -1,11 +1,22 @@
 package com.pluralsight;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GlobalMethods {
     static Scanner s = new Scanner(System.in);
     public static Integer ans(String message){
-        System.out.println(message);
-        return s.nextInt();
+        int n = -1;
+        do {
+            try {
+                System.out.println(message);
+                n = s.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("\nPlease enter a number option :)\n");
+                s.nextLine();
+            }
+        }while (n==-1);
+        return n;
     }
 }
