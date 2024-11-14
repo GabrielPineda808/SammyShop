c3f83a744ea3c46fe80b89346dfa7e14ea06ec33
Gabe <gabepineda6@gmail.com>
2024-11-14 05:45:26 -0800

    Placed prompt inside of a while loop to get valid respone rather than calling on the method again

--- src/main/java/com/pluralsight/Screens/HomeScreen.java
+++ src/main/java/com/pluralsight/Screens/HomeScreen.java
@@ -11,24 +11,26 @@ public class HomeScreen {
     public static void home() throws IOException {
         System.out.println("Welcome to our SammyShop\n");
 
-        int option = ans(
-                "Please select one of the following options below\n" +
-                "\n( 1 ) - Create New Order\n" +
-                "( 2 ) - Exit Application\n"
-        );
+        loop: while (true) {
+            int option = ans(
+                    "Please select one of the following options below\n" +
+                            "\n( 1 ) - Create New Order\n" +
+                            "( 2 ) - Exit Application\n"
+            );
 
-        switch (option){
-            case 1:
-                order();
-                break;
-            case 2:
-                System.out.println("Thank you for visiting SammyShop! Have a great day!\n");
-                exit(1);
-                break;
-            default:
-                System.out.println("Unfortunately that was not a valid option. Try again.\n");
-                home();
 
+            switch (option) {
+                case 1:
+                    order();
+                    break loop;
+                case 2:
+                    System.out.println("Thank you for visiting SammyShop! Have a great day!\n");
+                    exit(1);
+                    break loop;
+                default:
+                    System.out.println("Unfortunately that was not a valid option. Try again.\n");
+
+            }
         }
     }
 }
